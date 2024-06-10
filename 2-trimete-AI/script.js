6136597f31271020ca620be0a23609669adf65db
mariasakaue <106112130+mariasakaue@users.noreply.github.com>
2024-06-10 08:09:21 -0300

    Add files via upload

--- 2-trimete-AI/script.js
+++ 2-trimete-AI/script.js
@@ -3,3 +3,21 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const pergunta = [
+    {
+    enunciado: "Pergunta 1", 
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+ },
+ {
+    enunciado: "Pergunta 1", 
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+ }
+    
+];
